d04298c5acc552f2bf7635c0bb1660ed1d3c9f05
Dima <test@gmail.com>
2022-03-16 22:04:50 +0200

    fix2

--- .idea/src/Mail.java
+++ .idea/src/Mail.java
@@ -3,10 +3,6 @@ public class Mail {
         System.out.println("pc1");
         System.out.println("smthpc1");
         System.out.println("change 3");
-<<<<<<< HEAD
-        System.out.println("change 4");
-=======
-        System.out.println("change 3");
->>>>>>> 5c4f150cddf7b1bed4574d3a06e6d8b5ea563067
+
     }
 }
