a27c729187ffb14e1d415dd189d8504bcf4e495f
Dima <mr.tarasiuk@gmail.com>
2022-03-16 21:11:34 +0200

    3 commit

--- .idea/src/Mail.java
+++ .idea/src/Mail.java
@@ -2,5 +2,6 @@ public class Mail {
     public static void main(String[] args) {
         System.out.println("pc1");
         System.out.println("smthpc1");
+        System.out.println("change 3");
     }
 }
